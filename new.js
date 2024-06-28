fd726474792b4bc172c7745bb267d07db555a822
Devipriya N <124780929+DevipriyaNatarajan@users.noreply.github.com>
2024-06-28 21:06:26 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,30 @@
+/*n=1700;
+if((n%4==0)&&(n%100!=0) || (n%400==0))
+{
+  console.log("leap")
+}
+else{
+  console.log("not")
+}*/
+
+/*function arms(){
+  input=153;
+  output=153;
+  tot=0;
+  while(input>0){
+    n=input%10;
+    tot=tot+n*n*n;
+    input=input/10;
+  }
+  if(output=tot)
+  {
+    console.log("arms num")
+  }
+  else
+  {
+    console.log("not")
+  }
+}
+arms()*/
+
+
